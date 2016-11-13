16390c11f5d8299a0b3c62e38090283700f260a3
Stuart Bannerman <metalheadstorm@gmail.com>
2016-11-13 23:40:18 +0000

    pip setup file

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+"""
+"""
+
+from setuptools import setup
+
+setup(
+    name='PySpawner',
+    version='0.0.1',
+    description='Simple Python script that will spawn a given number of instances of the provided module',
+    author='Stuart Bannerman',
+    author_email='me@stuartbannerman.com',
+    license='MIT',
+    packages=[],
+    py_modules=['pyspawner'],
+    install_requires=[],
+    scripts=[],
+    include_package_data=True
+)
